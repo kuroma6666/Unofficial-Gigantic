790bdf30343945d3c5afdf25f260bf0dad4a15fe
kuroma6666 <kuroma.creator@gmail.com>
2025-02-16 17:53:13 +0900

    feat: `RankingEntitiy`に日間ランキングスコアのエンティティを追加

--- src/main/kotlin/click/seichi/gigantic/database/RankingEntity.kt
+++ src/main/kotlin/click/seichi/gigantic/database/RankingEntity.kt
@@ -2,6 +2,7 @@ package click.seichi.gigantic.database
 
 import click.seichi.gigantic.database.dao.ranking.RankingScore
 import click.seichi.gigantic.database.dao.ranking.RankingUser
+import click.seichi.gigantic.database.dao.ranking.DailyRankingScore
 import java.util.*
 
 /**
@@ -12,5 +13,7 @@ class RankingEntity(uniqueId: UUID) {
     val user = RankingUser.findById(uniqueId) ?: RankingUser.new(uniqueId) {}
 
     val score = RankingScore.findById(uniqueId) ?: RankingScore.new(uniqueId) {}
+
+    val dailyScore = DailyRankingScore.findById(uniqueId) ?: DailyRankingScore.new(uniqueId) {}
 
 }
